8eb452f36cdeb15ed536dfb47314443d203780a2
Ved Mistry <59229430+vedmistry-619@users.noreply.github.com>
2021-01-25 19:10:00 +0530

    Update walmart.py

--- walmart.py
+++ walmart.py
@@ -30,5 +30,54 @@ master_df['Quarter'] = master_df['Date'].dt.quarter
 master_df['Month'] = master_df['Date'].dt.month.apply(lambda x: calendar.month_abbr[x])
 master_df['Season'] = (master_df['Date'].apply(lambda dt: (dt.month%12 + 3)//3)).map(seasons_dict)
 
+result = sm.ols(formula="Weekly_Sales ~ isHoliday + Fuel_Price + Temperature + Size + MarkDown1 + MarkDown2 + MarkDown3 + MarkDown4 + MarkDown5 + CPI + Unemployment + Week_Number", data=master_df[(master_df['Store'] == 1)]).fit()
+print(result.summary())
+
 result = sm.ols(formula="Weekly_Sales ~ isHoliday + Fuel_Price + Temperature + Size + MarkDown1 + MarkDown2 + MarkDown3 + MarkDown4 + MarkDown5 + CPI + Unemployment + Week_Number", data=master_df[(master_df['Store'] == 2)]).fit()
 print(result.summary())
+
+result = sm.ols(formula="Weekly_Sales ~ isHoliday + Fuel_Price + Temperature + Size + MarkDown1 + MarkDown2 + MarkDown3 + MarkDown4 + MarkDown5 + CPI + Unemployment + Week_Number", data=master_df[(master_df['Store'] == 3)]).fit()
+print(result.summary())
+
+result = sm.ols(formula="Weekly_Sales ~ isHoliday + Fuel_Price + Temperature + Size + MarkDown1 + MarkDown2 + MarkDown3 + MarkDown4 + MarkDown5 + CPI + Unemployment + Week_Number", data=master_df[(master_df['Store'] == 4)]).fit()
+print(result.summary())
+
+result = sm.ols(formula="Weekly_Sales ~ isHoliday + Fuel_Price + Temperature + Size + MarkDown1 + MarkDown2 + MarkDown3 + MarkDown4 + MarkDown5 + CPI + Unemployment + Week_Number", data=master_df[(master_df['Store'] == 5)]).fit()
+print(result.summary())
+
+week_array = []
+store_array = []
+
+for i in range(1, 46):
+    store_array.append(i)
+total_stores_sales = {}
+
+for i in range(1, 46):
+    week_array.append(i)
+    current_store_array = []
+    for j in range(1, 53):
+        current_dp = master_df[(master_df['Store'] == i) & (master_df['Week_Number'] == j)]
+        sales_array = current_dp['Weekly_Sales'].tolist()
+        sum_sales = 0
+        for k in range(1, len(sales_array)):
+            sum_sales = sum_sales + sales_array[k]
+        current_store_array.append(sum_sales)
+    total_stores_sales[i] = current_store_array
+    
+store_array = []
+for i in range(1, 53):
+    store_array.append(i)
+
+weekly_total = []
+
+for i in range(52):
+    weekly_sum = 0
+    for j in range(1, 46):
+        weekly_sum = weekly_sum + total_stores_sales[j][i]
+    weekly_total.append(weekly_sum)
+
+sales_dict = {}
+for store in range(1, 46):
+    for dept in range(1, 100):
+        current_key = str(store) + "," + str(dept)
+        sales_dict[current_key] = 0
